b7fe8b5d53fb57b5f882812727625744b9b41465
Nasa8868 <raksa8868@gmail.com>
2025-10-14 17:14:58 +0700

    Updated by sa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-  return <div>App Component</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
---
+++ src/router/MainRouter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainRouter() {
+  return <div>MainRouter</div>;
+}
