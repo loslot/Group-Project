078c07c1a0c6cf5a98957f973204d02f4a6199e2
loslot <loslot100604@gmail.com>
2025-10-16 17:06:34 -0800

    update

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Homepage from "../page/Homepage";
-import About from "../page/About";
-import Detail from "../page/Detail";
 import NotFound_404 from "../error/NotFound_404";
 
 export default function MainRouter() {
@@ -10,8 +8,6 @@ export default function MainRouter() {
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Details" element={<Detail />} />
         <Route path="*" element={<NotFound_404 />} />
       </Routes>
     </div>
